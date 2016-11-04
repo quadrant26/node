bbb3943184047336299c572bd8335aa0b24140a3
quadrant26 <quadrant26@163.com>
2016-11-04 15:52:29 +0800

    test require exports

--- sum.js
+++ sum.js
@@ -7,6 +7,21 @@ function sum(a, b){
     return a + b
 }
 
+function sum2(m, n){
+
+    var temp = 0
+    if(m > n){
+        temp = m
+        n = temp
+        m = n
+    }
+    var result = 0
+    for(m; m < n; m++){
+        result += m
+    }
+    return result
+}
+
 var result = 0
 for(var i = 0; i < 100; i++){
     result += i
@@ -14,3 +29,4 @@ for(var i = 0; i < 100; i++){
 console.log(result)
 
 exports.sum = sum
+exports.sum2 = sum2
