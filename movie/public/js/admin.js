ffe46e36198d077247e486f072712ea5c4caae37
quadrant26 <quadrant26@163.com>
2016-11-22 18:45:57 +0800

    remove list msg

--- movie/public/js/admin.js
+++ movie/public/js/admin.js
@@ -6,15 +6,13 @@ $(function(){
 
     $(".del").click(function (e){
 
-        alert(234)
-
         var target = $(e.target)
         var id = target.data('id')
         var tr = $('.item-id-' + id)
 
         $.ajax({
             type : 'DELETE',
-            url : '/admin/list?id=' + id
+            url : '/admin/movie/list?id=' + id
         })
         .done(function (results){
             if(results.success === 1){
